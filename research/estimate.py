3061fe952a77f7c3d4a0d7988ff5b2f91a4f76b0
sotaro-ici <sotaro-ici@g.ecc.u-tokyo.ac.jp>
2024-11-12 18:34:26 +0900

    refactor the estimation script

--- research/estimate.py
+++ research/estimate.py
@@ -1,28 +1,33 @@
-from decimal import Decimal, getcontext
-from mpmath import mp
+from math import sqrt
+from rank import countsum
 
-getcontext().prec = 100
-mp.dps = 50
+S_all = countsum
+SAMPLE_ok = 40_491_613
+SAMPLE_all = 5_000_000_000
+print(f'|S_all| = {S_all}')
+print(f'SAMPLE_ok = {SAMPLE_ok}')
+print(f'SAMPLE_all = {SAMPLE_all}')
 
-S_all = mp.mpf('80880932079767835177773204009328769812438521503800714936366945233084532')
-s = mp.mpf('40491613')
-n = mp.mpf('5000000000')
+print('==========')
 
-p_hat = s / n
+p_hat = SAMPLE_ok / SAMPLE_all
+sigma_p_hat = sqrt((p_hat * (1 - p_hat)) / SAMPLE_all)
+print(f'sigma = {sigma_p_hat}')
 
-sigma = mp.sqrt((p_hat * (1 - p_hat)) / n)
-print(f'sigma = {sigma}')
-
-expected_S_ok = p_hat * S_all
-print(f'Expected |S_ok| calculated from the sample = {expected_S_ok}')
-
-p_upper = p_hat + 3 * sigma
-p_lower = p_hat - 3 * sigma
+p_upper = p_hat + 3 * sigma_p_hat
+p_lower = p_hat - 3 * sigma_p_hat
 print(f'p_upper = {p_upper}\np_lower = {p_lower}')
+
+print('==========')
 
 S_ok_upper = p_upper * S_all
 S_ok_lower = p_lower * S_all
-print(f'{S_ok_lower} <= |S_ok| <= {S_ok_upper}')
+print(f'CI: {S_ok_lower} <= |S_ok| <= {S_ok_upper}')
 
-error = S_all * 3 * sigma
-print(f'Error = {error}')
+margin_of_error = S_all * 3 * sigma_p_hat
+
+# margin_of_errorの指数部をS_allに合わせる
+# adjust the exponent of margin_of_error to match that of S_all
+exponent_S_ok = int('{:e}'.format(p_hat * S_all).split('e')[1])
+adjusted_significand_MoE = margin_of_error / (10 ** exponent_S_ok)
+print('MoE = ±{:.10f}e{:+d}'.format(adjusted_significand_MoE, exponent_S_ok))
