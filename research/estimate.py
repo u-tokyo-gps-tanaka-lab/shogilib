7347378286140a7e27d24be0115dfde1c3ebf7af
sotaro-ici <sotaro-ici@g.ecc.u-tokyo.ac.jp>
2024-11-12 21:08:57 +0900

    refine outputs from the estimation script

--- research/estimate.py
+++ research/estimate.py
@@ -1,12 +1,11 @@
 from math import sqrt
-from rank import countsum
+from rank import countsum as S_all
 
-S_all = countsum
 SAMPLE_ok = 40_491_613
 SAMPLE_all = 5_000_000_000
 print(f'|S_all| = {S_all}')
-print(f'SAMPLE_ok = {SAMPLE_ok}')
-print(f'SAMPLE_all = {SAMPLE_all}')
+print(f'|SAMPLE_ok| = {SAMPLE_ok}')
+print(f'|SAMPLE_all| = {SAMPLE_all}')
 
 print('==========')
 
@@ -20,14 +19,18 @@ print(f'p_upper = {p_upper}\np_lower = {p_lower}')
 
 print('==========')
 
+expected_S_ok = p_hat * S_all
 S_ok_upper = p_upper * S_all
 S_ok_lower = p_lower * S_all
 print(f'CI: {S_ok_lower} <= |S_ok| <= {S_ok_upper}')
 
 margin_of_error = S_all * 3 * sigma_p_hat
 
-# margin_of_errorの指数部をS_allに合わせる
-# adjust the exponent of margin_of_error to match that of S_all
-exponent_S_ok = int('{:e}'.format(p_hat * S_all).split('e')[1])
+significand_S_ok = '{:}'.format(expected_S_ok).split('e')[0]
+exponent_S_ok = int('{:}'.format(expected_S_ok).split('e')[1])
+
+# margin_of_errorの指数部をS_okに合わせる
+# adjust the exponent of margin_of_error to match that of S_ok
 adjusted_significand_MoE = margin_of_error / (10 ** exponent_S_ok)
-print('MoE = ±{:.10f}e{:+d}'.format(adjusted_significand_MoE, exponent_S_ok))
+
+print('|S_ok| ~ ({:} ± {:.10f})e{:+d}'.format(significand_S_ok, adjusted_significand_MoE, exponent_S_ok))
