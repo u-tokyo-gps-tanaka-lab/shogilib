08acc9afa1f2919eaa2bfe6861a9df8358b38adc
sotaro-ici <sotaro-ici@g.ecc.u-tokyo.ac.jp>
2024-11-19 14:41:39 +0900

    update estimation formats

--- research/estimate.py
+++ research/estimate.py
@@ -22,15 +22,15 @@ print('==========')
 expected_S_ok = p_hat * S_all
 S_ok_upper = p_upper * S_all
 S_ok_lower = p_lower * S_all
-print(f'CI: {S_ok_lower} <= |S_ok| <= {S_ok_upper}')
+print(f'99.73% CI: {S_ok_lower} <= |S_ok| <= {S_ok_upper}')
 
 margin_of_error = S_all * 3 * sigma_p_hat
 
-significand_S_ok = '{:}'.format(expected_S_ok).split('e')[0]
-exponent_S_ok = int('{:}'.format(expected_S_ok).split('e')[1])
+significand_S_ok = '{}'.format(expected_S_ok).split('e')[0]
+exponent_S_ok = int('{}'.format(expected_S_ok).split('e')[1])
 
 # margin_of_errorの指数部をS_okに合わせる
 # adjust the exponent of margin_of_error to match that of S_ok
 adjusted_significand_MoE = margin_of_error / (10 ** exponent_S_ok)
 
-print('|S_ok| ~ ({:} ± {:.10f})e{:+d}'.format(significand_S_ok, adjusted_significand_MoE, exponent_S_ok))
+print('|S_ok| ~ ({} ± {:.10f})e{:+d}'.format(significand_S_ok, adjusted_significand_MoE, exponent_S_ok))
